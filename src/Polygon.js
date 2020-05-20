a06980e9e1d0566a112dc0b5571658c68b8b6c53
daewoongBang <daewoong.bang@gmail.com>
2020-05-20 13:39:39 +0900

    cleanup & before update  4.0 beta

--- src/Polygon.js
+++ src/Polygon.js
@@ -23,6 +23,93 @@ const Polygon = () => {
 
   const handleEditPolygonButtonClick = () => {
     window.console.log('Edit');
+    const activePolygon = canvas.getActiveObject();
+
+    if (activePolygon) {
+      canvas.setActiveObject(activePolygon);
+      activePolygon.edit = !activePolygon.edit;
+
+      if (activePolygon.edit) {
+        const lastControl = activePolygon.points.length - 1;
+        activePolygon.cornerStyle = 'circle';
+        // activePolygon.controls = activePolygon.points.reduce(
+        //   (acc, point, index) => {
+        //     acc['p' + index] = new fabric({
+        //       positionHandler: polygonPositionHandler,
+        //       actionHandler: anchorWrapper(
+        //         index > 0 ? index - 1 : lastControl,
+        //         actionHandler
+        //       ),
+        //       actionName: 'modifyPoligon',
+        //       pointIndex: index
+        //     });
+        //     return acc;
+        //   },
+        //   {}
+        // );
+      } else {
+        activePolygon.cornerStyle = 'rect';
+        activePolygon.controls = fabric.Object.prototype.controls;
+      }
+      activePolygon.hasBorders = !activePolygon.edit;
+      canvas.requestRenderAll();
+
+      window.console.log(activePolygon);
+    }
+  };
+
+  const polygonPositionHandler = (dim, finalMatrix, fabricObject) => {
+    const x =
+        fabricObject.points[this.pointIndex].x - fabricObject.pathOffset.x,
+      y = fabricObject.points[this.pointIndex].y - fabricObject.pathOffset.y;
+    return fabric.util.transformPoint(
+      { x: x, y: y },
+      fabricObject.calcTransformMatrix()
+    );
+  };
+
+  const actionHandler = (eventData, transform, x, y) => {
+    const polygon = transform.target,
+      currentControl = polygon.controls[polygon.__corner],
+      mouseLocalPosition = polygon.toLocalPoint(
+        new fabric.Point(x, y),
+        'center',
+        'center'
+      ),
+      size = polygon._getTransformedDimensions(0, 0),
+      finalPointPosition = {
+        x:
+          (mouseLocalPosition.x * polygon.width) / size.x +
+          polygon.pathOffset.x,
+        y:
+          (mouseLocalPosition.y * polygon.height) / size.y +
+          polygon.pathOffset.y
+      };
+    polygon.points[currentControl.pointIndex] = finalPointPosition;
+    return true;
+  };
+
+  const anchorWrapper = (anchorIndex, fn) => {
+    return function (eventData, transform, x, y) {
+      const fabricObject = transform.target,
+        absolutePoint = fabric.util.transformPoint(
+          {
+            x: fabricObject.points[anchorIndex].x - fabricObject.pathOffset.x,
+            y: fabricObject.points[anchorIndex].y - fabricObject.pathOffset.y
+          },
+          fabricObject.calcTransformMatrix()
+        );
+      const actionPerformed = fn(eventData, transform, x, y);
+      const newDim = fabricObject._setPositionDimensions({});
+      const newX =
+          (fabricObject.points[anchorIndex].x - fabricObject.pathOffset.x) /
+          fabricObject.width,
+        newY =
+          (fabricObject.points[anchorIndex].y - fabricObject.pathOffset.y) /
+          fabricObject.height;
+      fabricObject.setPositionByOrigin(absolutePoint, newX + 0.5, newY + 0.5);
+      return actionPerformed;
+    };
   };
 
   const handleMouseUp = useCallback(
@@ -96,7 +183,10 @@ const Polygon = () => {
         top: getTopPosition(polygonPoints),
         fill: 'rgba(0, 255, 0, 0.1)',
         stroke: 'rgba(0, 255, 0, 0.5)',
-        strokeWidth: 2
+        strokeWidth: 2,
+        hasBorders: false,
+        hasControls: false
+        // evented: false
       });
 
       return polygon;
@@ -108,9 +198,10 @@ const Polygon = () => {
   const getTopPosition = useCallback(
     points => {
       let result = canvas.height;
-      for (let f = 0; f < lineCount; f++) {
-        if (points[f].y < result) {
-          result = points[f].y;
+
+      for (let i = 0; i < lineCount; i++) {
+        if (points[i].y < result) {
+          result = points[i].y;
         }
       }
       return Math.abs(result);
@@ -167,7 +258,9 @@ const Polygon = () => {
     if (canvas) {
       setCanvas(canvas);
     } else {
-      const fabricCanvas = new fabric.Canvas(canvasEl.current, {});
+      const fabricCanvas = new fabric.Canvas(canvasEl.current, {
+        // selection: false
+      });
       fabricCanvas
         .setWidth(500)
         .setHeight(500)
