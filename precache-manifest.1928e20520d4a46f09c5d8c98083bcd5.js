abb1f82764dd68b085ffc886e932377188a6b120
daewoongBang <daewoong.bang@gmail.com>
2020-06-01 16:07:41 +0900

    Updates

---
+++ precache-manifest.1928e20520d4a46f09c5d8c98083bcd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2672ae26a93f6e17670e32eeb1a7413d",
+    "url": "/fabric-react/index.html"
+  },
+  {
+    "revision": "6f8f1c33d51d2bbe9950",
+    "url": "/fabric-react/static/js/2.91025ef4.chunk.js"
+  },
+  {
+    "revision": "74549c08325e899dd17d787a1ff374cf",
+    "url": "/fabric-react/static/js/2.91025ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40461e7d9f2ea9e966ea",
+    "url": "/fabric-react/static/js/main.d6b835ea.chunk.js"
+  },
+  {
+    "revision": "7cda52bde687d28b5d7d",
+    "url": "/fabric-react/static/js/runtime-main.085ca109.js"
+  }
+]);
